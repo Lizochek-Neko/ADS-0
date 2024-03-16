088a03cd00f4d2bbef33c37e57e71588025d4115
Lizochek-Neko <160259786+Lizochek-Neko@users.noreply.github.com>
2024-03-16 22:13:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-	for (int i = a / 2; i >= 2; i--) {
-		if (a % i == 0 && b % i == 0) {
-			return i;
-		}
-	}
-	return 1;
+    for (int i = a / 2; i >= 2; i--) {
+        if (a % i == 0 && b % i == 0) {
+            return i;
+        }
+    }
+    return 1;
 }
